e310d80f041373859f52c769b533c100987b4a15
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-17 09:22:14 +0530

    2 way binding

--- src/app/viewall/viewall.component.ts
+++ src/app/viewall/viewall.component.ts
@@ -10,9 +10,10 @@ export class ViewallComponent implements OnInit {
   constructor() { }
 
   name="Saranya"
-  
+  status:boolean=false
   readValue=()=>{
     this.name="Raju"
+    this.status=true
   }
 
   ngOnInit(): void {
