71d338fec9f66047cf9ff997c6ab506457e62ae0
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-17 08:58:31 +0530

    viewall

---
+++ src/app/viewall/viewall.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewall',
+  templateUrl: './viewall.component.html',
+  styleUrls: ['./viewall.component.css']
+})
+export class ViewallComponent implements OnInit {
+
+  constructor() { }
+
+  name="Saranya"
+
+  ngOnInit(): void {
+  }
+
+}
